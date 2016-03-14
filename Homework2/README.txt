595d2d8fa47258a717499e24665a078c8e685008
Joshua Fram <jpf2141@columbia.edu>
2016-03-13 22:00:52 -0400

    changed sample size in test, readme fixes, committed .pyc files

--- Homework2/README.txt
+++ Homework2/README.txt
@@ -3,6 +3,8 @@ jpf2141
 
 1) Dependency Graphs
 
+a) See images in HW2 root directory
+
 b) In the dependency graph of a projective sentence, there are no edges that cross over one another. Therefore, by checking to see that there are not any word-to-word edges that cross with another word-to-word edge, we can make sure that the sentence is projective.
 
 c)
--- Homework2/test.py
+++ Homework2/test.py
@@ -3,16 +3,30 @@ from providedcode.transitionparser import TransitionParser
 from providedcode.evaluate import DependencyEvaluator
 from featureextractor import FeatureExtractor
 from transition import Transition
+import time
+import random
+
+
 
 if __name__ == '__main__':
     traindata = dataset.get_swedish_train_corpus().parsed_sents()
 
+#generate random sampling of training data
+    sample_size = 10
+    time_seed = lambda: int(round(time.time() * 1000))
+    random.seed(time_seed)
+    train_sample = random.sample(traindata, sample_size)
+
 
     try:
 
-        # tp = TransitionParser(Transition, FeatureExtractor)
-        # tp.train(traindata)
+        print "0"
+        tp = TransitionParser(Transition, FeatureExtractor)
+        print "1"
+        tp.train(train_sample)
+        print "2"
         # tp.save('swedish.model')
+        print "3"
 
         labeleddata = dataset.get_swedish_dev_corpus().parsed_sents()
         blinddata = dataset.get_swedish_dev_blind_corpus().parsed_sents()
